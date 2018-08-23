b5129b49962593723a3182666e3a860ff22f1d18
MarietaKocharyan <98marikoch@gmail.com>
2018-08-23 12:39:36 +0400

    Found the solution of two multiple numbers

---
+++ script.js
@@ -0,0 +1,8 @@
+   function multiple(x) {
+      if ((x % 3 == 0) || (x % 7 == 0)) {  //This line is check is number multiple
+        alert("check");
+      }else{
+          alert("failed");
+      }
+    }
+    multiple(21); //10,3
