32aa0281469694e704d94f7bbec5855b983ab7b8
MarietaKocharyan <31045697+MarietaKocharyan@users.noreply.github.com>
2018-08-23 13:57:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function multiple(x) {
-    if ((x % 3 == 0) || (x % 7 == 0)) {  //This line is check is number multiple
-        alert("check");
-    }else{
-        alert("failed");
-    }
+   ((x % 3 == 0) || (x % 7 == 0)) ? alert("check") : alert("failed");
 }
 multiple(21); //10,3
