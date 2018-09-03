43982e35d96f9b040f2b637e5397b17d37312f9f
MarietaKocharyan <98marikoch@gmail.com>
2018-09-03 20:19:10 +0400

    Soluthion of 3rd task, using some method's for soluthion

--- script.js
+++ script.js
@@ -200,9 +200,27 @@ function difference(first, second) {
     let value = [];
     for ( let i = 0; i < first.length; i++) {
         for (let j = 0; j < second.length; j++) {
-            if (first[i] == second[j]){
-            return value;}
+            if (first[i] == second[j]) {
+                value.push ( first[i] )
+            }
         }
     }
+    return value;
 }
 console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+
+function differenceOf2Arrays (array1, array2) {
+    let temp = [];
+    array1 = array1.toString().split(',').map(Number);
+    array2 = array2.toString().split(',').map(Number);
+         for (let in array1) {
+             if (array2.indexOf(array1[i]) === -1) temp.push(array1[i]);
+        }
+         for(i in array2) {
+            if(array1.indexOf(array2[i]) === -1) temp.push(array2[i]);
+         }
+     return temp.sort((a,b) => a-b);
+}
+console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
+
+
